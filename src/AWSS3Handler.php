43268429061a35570bd668606cf6662eec85ab53
Jeff Ho <jeffho@weblisher.com.tw>
2024-08-11 03:25:03 +0000

    add copyObject function

--- src/AWSS3Handler.php
+++ src/AWSS3Handler.php
@@ -105,6 +105,43 @@ class AWSS3Handler
     }
 
     /**
+     * 複製存储桶中的对象
+     *
+     * @param S3Client $client S3 客户端
+     * @param string $bucketKey 存储桶配置鍵
+     * @param string $objectKey 新对象键
+     * @param string $copySource 複製對象源路径
+     * @return \stdClass 返回包含上传结果或错误信息的响应对象
+     */
+    public function copyObject(S3Client $client, string $bucketKey, string $objectKey, string $copySource): \stdClass
+    {
+        $response = new \stdClass();
+        try {
+            $config = $this->getConfig($bucketKey);
+            if (!isset($config)) {
+                throw new \Exception('無效的配置鍵');
+            }
+            $destinationKey = $config['root_path'] . $objectKey;
+            $sourceKey = $this->getObjectKey($copySource);
+            $sourceBucket = $this->getObjectBucket($sourceKey);
+            $result = $client->copyObject([
+                'Bucket' => $config['bucket'],
+                'Key' => $destinationKey,
+                'CopySource' => "{$sourceBucket}/{$sourceKey}",
+            ]);
+            $response->status = 'success';
+            $response->object = $destinationKey;
+        } catch (AwsException $e) {
+            $response->status = 'failure';
+            $response->message = $e->getMessage();
+        } catch (\Exception $e) {
+            $response->status = 'failure';
+            $response->message = $e->getMessage();
+        }
+        return $response;
+    }
+
+    /**
      * 删除存储桶中的对象
      *
      * @param S3Client $client S3 客户端
@@ -169,6 +206,49 @@ class AWSS3Handler
 
         // 检查扩展名是否在允许的列表中
         return in_array($fileExtension, $allowedExtensions, true);
+    }
+
+    /**
+     * 获取对象的儲存桶
+     *
+     * @param string $objectKey 物件對象鍵
+     * @return string 对象的儲存桶
+     */
+    public function getObjectBucket(string $objectKey)
+    {
+        $segments = explode('/', $objectKey);
+        $key = $segments[0];
+        // 取得配置文件
+        $config = $this->getConfig($key);
+        // 從配置文件取得儲存桶, 若設定檔沒有訊息, 則為舊儲存桶
+        return $config['bucket'] ?? 'holkee';
+    }
+
+    /**
+     * 获取对象的鍵值
+     *
+     * @param string $source 源路径
+     * @return string 对象的鍵值
+     */
+    public function getObjectKey(string $source)
+    {
+        if (preg_match('/(https?:)?\/\/img\.holkee\.com/', $source)) {
+            // 定義正規表達式
+            $pattern = '/(https?:)?\/\/img\.holkee\.com/';
+            // 使用 preg_replace 進行替換
+            $result = preg_replace($pattern, 'images', $string);
+        } else {
+            // 定義正規表達式
+            $pattern = '/(https?:)?\/\/(cdn|cloud)\.holkee\.com\//';
+            // 使用 preg_replace 進行替換
+            $result = preg_replace($pattern, '', $string);
+        }
+        // 如果替換後的字串是以 'site/' 開頭，將其替換為 'image/site/'
+        if (strpos($result, 'site/') === 0) {
+            $result = 'images/' . $result;
+        }
+        // 回傳替換後的結果
+        return $result;
     }
 
     /**
